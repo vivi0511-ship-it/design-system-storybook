179ffdf48eecbda100ba60d6c40589d9882d4aef
Shaivi Lavhe <shaivilavhe@gmail.com>
2026-09-04 22:45:54 +0530

    chore(components): remove unused sections Component 315, Component 336, Date filters, Filters, Map, and Zones

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-// Preserved Figma Design System Barrel Export
-export * from './components/Component 336/Component 336';
-export * from './components/Filters/Filters';
-export * from './components/Component 315/Component 315';
-export * from './components/Date filters/Date filters';
-export * from './components/Zones/Zones';
-export * from './components/Map/Map';
-
 // Figma Discovered Design System Components
 export * from './components/Buttons/Buttons';
 export * from './components/Icons Buttons/Icons Buttons';
@@ -22,4 +14,3 @@ export * from './components/Questionnaire card/Questionnaire card';
 export * from './components/Arrival card/Arrival card';
 export * from './components/Incident heat map card/Incident heat map card';
 export * from './components/PillComponentGrid/PillComponentGrid';
-
